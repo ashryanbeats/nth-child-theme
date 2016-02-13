4a31dc6c5666a0535b4db53294f9fe9747bfabbd
Ash <ash@ashryanbeats.com>
2016-02-13 15:33:36 -0500

    Gulp watching for html and css changes. Livereload for both. Compiling sass on gulp start.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
 var runSeq = require('run-sequence');
 
 gulp.task('default', function() {
+	livereload.listen();
+	gulp.start('build');
+
 	gulp.watch(['sass/main.scss', 'sass/**/*.scss'], function() {
-        runSeq('buildCSS');
+        runSeq('buildCSS', 'reload');
     });
+
+    gulp.watch(['index.html'], function() {
+    	runSeq('reload');
+    })
+});
+
+gulp.task('build', function() {
+    runSeq(['buildCSS']);
 });
 
 gulp.task('buildCSS', function() {
@@ -16,3 +28,7 @@ gulp.task('buildCSS', function() {
         .pipe(rename('style.css'))
         .pipe(gulp.dest('./'));
 });
+
+gulp.task('reload', function() {
+    livereload.reload();
+})
