a9e94d3280b0a5d15bd7190c69ec77b22ad0c3d3
Ash <ash@ashryanbeats.com>
2016-02-14 17:10:50 -0500

    Dynamic copyright year.

--- footer.php
+++ footer.php
@@ -9,8 +9,8 @@
 				<div class="container">
 					<div class="row copyright"> <!-- copyright widget row -->
 						<div class="col-md-12">
-							<small><p>nth-child theme for WordPress made with ア! by Ash Ryan.</p></small>
-							<small><p>©2010 – 2016 Ash Ryan.</p></small>
+							<small><p><a href="https://github.com/ashryanbeats/nth-child-theme/tree/master" alt="nth-child theme on GitHub">nth-child theme for WordPress</a> made with ア! by Ash Ryan.</p></small>
+							<small><p><?php echo '©2010 – '.date("Y") ?> Ash Ryan.</p></small>
 						</div>
 					</div> <!-- copyright widget row -->
 				</div>
