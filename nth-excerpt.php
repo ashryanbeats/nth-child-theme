05b4a80ab002ba2f94e7c578920d894ebab620c5
Ash <ash@ashryanbeats.com>
2016-02-15 12:04:30 -0500

    Template for post title and byline.

--- nth-excerpt.php
+++ nth-excerpt.php
@@ -1,8 +1,10 @@
 <article>
 	<a href="<?php the_permalink() ?>" title="<?php the_title() ?>">
-		<h2><?php the_title() ?></h2>
-		<p class="post-date"><time datetime="<?php echo get_the_date( 'Y-m-d' ); ?>"><?php echo get_the_date( 'D, M j Y' ); ?></time></p>
+
+		<?php locate_template('nth-title-byline.php', true, false) ?>	
 		<?php the_post_thumbnail('blog-thumb'); ?>
+
 	</a>
+	
 	<?php the_excerpt() ?>
 </article>	
---
+++ nth-title-byline.php
@@ -0,0 +1,2 @@
+<h2><?php the_title() ?></h2>
+<p class="post-date"><time datetime="<?php echo get_the_date( 'Y-m-d' ); ?>"><?php echo get_the_date( 'D, M j Y' ); ?></time></p>
