9c877ae97772b4845aa8ef112af9cc74cb0c26e7
Ash <ash@ashryanbeats.com>
2016-02-14 20:30:12 -0500

    Added page.php.

--- page.php
+++ page.php
@@ -1,13 +1,18 @@
 <?php get_header() ?>
 
-<div class="col-md-10 excerpts"> <!-- article column -->
-	<article>
-		<h2><a href="#" alt="Title">Title</a></h2>
-		<img class="img-responsive" src="http://i0.wp.com/ashryanbeats.com/wp-content/uploads/2016/01/image.jpeg">
-		<p>Lorem ipsum Elit minim adipisicing esse sit irure proident fugiat Duis aliqua sint est.</p>
-	</article>
+<div class="col-md-10 page"> <!-- article column -->
+	<?php while ( have_posts()) : the_post(); ?>
 
+		<article>
+			<h1><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h1>
+			<?php the_post_thumbnail(); ?>
+
+			<!-- Show the feature image caption -->
+			<?php get_featured_image_caption() ?>
+			<?php the_content() ?>
+		</article>
+
+	<?php endwhile; ?>
 </div> <!-- article column -->
-
 	
 <?php get_sidebar() ?>
--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php get_header() ?>
 
-<div class="col-md-10 "> <!-- article column -->
+<div class="col-md-10 single"> <!-- article column -->
 	<?php while ( have_posts()) : the_post(); ?>
 
 		<article>
