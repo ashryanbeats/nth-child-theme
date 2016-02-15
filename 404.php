f82f8a93875eec85575971671c69abfeacb7968d
Ash <ash@ashryanbeats.com>
2016-02-14 20:28:51 -0500

    Article title on single.php is h1.

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 
 <div class="col-md-10 404"> <!-- article column -->
 	<h3>What you seek is not here.</h3>
-	<p><a href="<?php echo home_url() ?>" alt="<?php bloginfo('name') ?>">Perhaps it is here?</a></p>
+	<p><a href="<?php echo home_url() ?>" title="<?php bloginfo('name') ?>">Perhaps it is here?</a></p>
 </div>
 	
 <?php get_sidebar() ?>
--- single.php
+++ single.php
@@ -4,7 +4,7 @@
 	<?php while ( have_posts()) : the_post(); ?>
 
 		<article>
-			<h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+			<h1><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h1>
 			<?php the_post_thumbnail(); ?>
 
 			<!-- Show the feature image caption -->
