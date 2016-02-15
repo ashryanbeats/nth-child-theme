8c8f6a88d6b2a758fffab92854834e7aab48c196
Ash <ash@ashryanbeats.com>
2016-02-15 12:18:30 -0500

    Added permalink to full article titles and images.

--- nth-full-article.php
+++ nth-full-article.php
@@ -1,8 +1,9 @@
 <article>
-	<?php locate_template('nth-title-byline.php', true, false) ?>
-	<?php the_post_thumbnail(); ?>
+	<a href="<?php the_permalink() ?>" title="<?php the_title() ?>">
+		<?php locate_template('nth-title-byline.php', true, false) ?>
+		<?php the_post_thumbnail(); ?>
+		<?php get_featured_image_caption() ?>
+	</a>
 
-	<!-- Show the feature image caption -->
-	<?php get_featured_image_caption() ?>
 	<?php the_content() ?>
 </article>
